2784e666459dd2799446e6f989a6c5cf8853fa31
Francisco <franciscocruz040425@gmail.com>
2024-12-20 17:42:52 -0400

    vite.config added

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+
+base:'/blackjack-vite/'
+
+});
+
+
+// se uso para desplegar en git page  por el uso de la ruta de despligue 
+//investigar mas en la documentaion de vite 
